467391cc244d7761d6c9172a9cdb43f69673a1f7
Jonathan Itzen <jonathanitzen95@gmail.com>
2018-07-21 16:22:07 -0500

    stuff

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-AOS.init();
+window.addEventListener('load', function() {
+  AOS.init();
 
-document.querySelector('.hero-image i').addEventListener('click', function() {
-  this.classList.toggle('rotate')
-	document.querySelector('.hero-image img').classList.toggle('move-left')
+  document.querySelector('.hero-image i').addEventListener('click', function() {
+    this.classList.toggle('rotate')
+  	document.querySelector('.hero-image img').classList.toggle('move-left')
+  })
+
 })
