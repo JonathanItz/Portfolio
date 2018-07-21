69bf7591c323f62a5b8a35b63da04308b7e8b866
Jonathan Itzen <jonathanitzen95@gmail.com>
2018-07-21 12:41:05 -0500

    init commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+AOS.init();
+
+document.querySelector('.hero-image i').addEventListener('click', function() {
+  this.classList.toggle('rotate')
+	document.querySelector('.hero-image img').classList.toggle('move-left')
+})
